275cea1ceec0ee1f6adfdaf85083b371cb9e49b8
Kristina Vlasovets <kristinavlasovets@K-Mac.Dlink>
2023-04-28 14:52:07 +0300

    last

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import {Chat} from 'stream-chat-react';
-import {Box, Button} from '@mui/material';
-import {JoinGame} from '../components/JoinGame';
+import React from "react";
+import { Chat } from "stream-chat-react";
+import { Box, Button } from "@mui/material";
+import { JoinGame } from "../components/JoinGame";
 
-export const Join = ({setIsAuth, client, user}) => {
-	const handleLogout = () => {
-		localStorage.removeItem('token');
-		localStorage.removeItem('userId');
-		localStorage.removeItem('username');
-		setIsAuth(false);
-		client.disconnectUser();
-	};
-	return (
-		<Box>
-			<Chat client={client}>
-				<Button
-					sx={{m: '20px 20px', width: 100}}
-					type="button"
-					variant="contained"
-					onClick={handleLogout}
-					color="error"
-				>
-					Logout
-				</Button>
+export const Join = ({ setIsAuth, client, user }) => {
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("username");
+    setIsAuth(false);
+    client.disconnectUser();
+  };
+  return (
+    <Box>
+      <Chat client={client}>
+        <Button
+          sx={{ m: "20px 20px", width: 100 }}
+          type="button"
+          variant="contained"
+          onClick={handleLogout}
+          color="error"
+        >
+          Logout now
+        </Button>
 
-				<JoinGame user={user} handleLogout={handleLogout} />
-			</Chat>
-		</Box>
-	);
+        <JoinGame user={user} handleLogout={handleLogout} />
+      </Chat>
+    </Box>
+  );
 };
